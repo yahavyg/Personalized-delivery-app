66b9fb2b422bad8358abd59a04ff5b5ce13d7758
Yahav Yosef Gal <yahav.yg@gmail.com>
2025-07-21 11:00:16 +0300

    test

---
+++ app/src/main/java/com/yahav/deliverycustomer/MyFirebaseMessagingService.java
@@ -0,0 +1,12 @@
+
+package com.yahav.deliverycustomer;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage message) {
+        // handle notification
+    }
+}
---
+++ app/src/main/java/com/yahav/deliverycustomer/Order.java
@@ -0,0 +1,7 @@
+
+package com.yahav.deliverycustomer;
+
+public class Order {
+    public String id, userId, pickup, destination, clientName, clientPhone, status;
+    public double price;
+}
